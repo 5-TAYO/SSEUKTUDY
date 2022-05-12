167cfa1fb2dc697d75aebd2e3bd2339f413a1795
Yeom-JinHo <pwlsghq@naver.com>
2022-05-13 00:50:49 +0900

    test : sass & carco 테스트

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,18 @@
 import "./App.css";
 import React from "react";
+import Main from "@screens/Main";
+
+import "@styles/reset.css";
+import "@styles/_typography.scss";
+import "@styles/_common.scss";
+import "@styles/font.css";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
 }
 
 export default App;
---
+++ frontend/src/screens/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import MainComponent from "@components/MainComponent";
+
+function Main() {
+  return (
+    <div>
+      여기는 메인 페이지입니다.
+      <MainComponent />
+    </div>
+  );
+}
+
+export default Main;
