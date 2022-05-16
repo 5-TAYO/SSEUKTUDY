d4f78303555948895009483a89ab006daaca3453
Yeom-JinHo <81306489+Yeom-JinHo@users.noreply.github.com>
2022-05-17 00:42:33 +0900

    setup : 리액트 프로젝트 기본 셋팅 (#3)
    * setup : craco & sass & uuid 세팅 #1
    
    * feat : 기본 스타일/유틸  셋팅 #1
    
    * test : sass & carco 테스트
    
    * setup : font 추가 #1
    
    * setup : react router 추가 #1
    
    * setup : 기본 라우터 파일구조 #1
    
    * setup : 색깔이랑 폰트 추가 #1
    
    * setup : 로고이미지 추가
    
    * remove : 이미지 삭제 #1
    
    * setup : 이미지 다시 추가
    
    Co-authored-by: jmlee9707 <jmlee9707@naver.com>

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,13 @@
 import "./App.css";
 import React from "react";
+import "@styles/reset.css";
+import "@styles/_typography.scss";
+import "@styles/_common.scss";
+import "@styles/font.css";
+import Router from "./routers/Router";
 
 function App() {
-  return <div className="App" />;
+  return <Router />;
 }
 
 export default App;
---
+++ frontend/src/components/MainComponent.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MainComponent() {
+  return <p className="fs-16">메인 컴포넌트!!</p>;
+}
+
+export default MainComponent;
