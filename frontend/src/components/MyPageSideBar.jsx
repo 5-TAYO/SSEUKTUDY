9cc5f918daca041cfbd6c5a4b45a12fb2a96981d
choijinhap <wlsgkq123@gmail.com>
2022-05-17 02:24:07 +0900

    style : 마이페이지 사이드바 스타일 고침

--- frontend/src/components/MyPageSideBar.jsx
+++ frontend/src/components/MyPageSideBar.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { ReactComponent as Avatar } from "../assets/images/Avatar.svg";
+import { ReactComponent as Avatar } from "@images/Avatar.svg";
 import "./MyPageSideBar.scss";
 
 function MyPageSideBar() {
   return (
-    <div className="mypage-sidebar">
+    <div className="mypage-sidebar notoMid">
       <div className="profile">
         <Avatar className="profile-mini-img" />
-        <div className="name fs-24">이정민</div>
+        <div className="name fs-24 notoBold">이정민</div>
         <div className="email fs-12">jmlee907@nate.com</div>
         <div className="email fs-12">프로필 수정</div>
       </div>
@@ -16,16 +16,16 @@ function MyPageSideBar() {
         <div className="menu-title fs-16">쪽지함</div>
         <hr />
         <div className="sub-menu">
-          <NavLink to="/" className="sub-menu-title fs-13">
+          <NavLink to="recv" className="sub-menu-title fs-13">
             받은 쪽지함
           </NavLink>
-          <NavLink to="/" className="sub-menu-title  fs-13">
+          <NavLink to="send" className="sub-menu-title  fs-13">
             보낸 쪽지함
           </NavLink>
         </div>
         <hr />
         <div className="sub-menu">
-          <NavLink to="/MyPage" className="sub-menu-title  fs-13">
+          <NavLink to="group" className="sub-menu-title  fs-13">
             스터디 단체 쪽지함
           </NavLink>
         </div>
