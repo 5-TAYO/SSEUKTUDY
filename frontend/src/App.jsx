13de3ed663c74b836915677d94cdb5e918d4128a
Yeom-JinHo <pwlsghq@naver.com>
2022-05-16 22:16:07 +0900

    setup : react router 추가 #1

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,18 +1,13 @@
 import "./App.css";
 import React from "react";
-import Main from "@screens/Main";
-
 import "@styles/reset.css";
 import "@styles/_typography.scss";
 import "@styles/_common.scss";
 import "@styles/font.css";
+import Router from "./routers/Router";
 
 function App() {
-  return (
-    <div className="App">
-      <Main />
-    </div>
-  );
+  return <Router />;
 }
 
 export default App;
