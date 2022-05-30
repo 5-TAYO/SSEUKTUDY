faa9a8e1df275a731ca044be912694f386c17009
Yeom-JinHo <pwlsghq@naver.com>
2022-05-30 19:22:12 +0900

    feat : 홈페이지 퍼블리싱 마무리 #9

--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import DummyImg from "@images/HomeDummy.png";
 import "./Home.scss";
+import PropTypes from "prop-types";
 
 function Home() {
   return (
@@ -34,9 +35,49 @@ function Home() {
           스윽터디에 대해 알려드릴게요
         </p>
         <p className="main__desc--emphasis notoBold fs-24">집중</p>
+        <div className="main__card-cotainer flex">
+          <HomeDescCard
+            title="직접만들고"
+            desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique varius penatibus tellus leo nibh arcu. "
+          />
+          <HomeDescCard
+            title="직접만들고"
+            desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique varius penatibus tellus leo nibh arcu. "
+          />
+          <HomeDescCard
+            title="직접만들고"
+            desc="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tristique varius penatibus tellus leo nibh arcu. "
+          />
+        </div>
+        <div className="logo-container flex align-center">
+          <div className="logo" />
+          <div className="p-container flex column">
+            <p className="logo-desc-top fs-32 notoBold">
+              완벽한 스터디를 위하여
+            </p>
+            <p className="logo-desc-bottom fs-32 notoBold">스윽터디</p>
+          </div>
+        </div>
       </main>
     </div>
   );
 }
+
+function HomeDescCard({ title, desc }) {
+  return (
+    <div className="main__desc-card flex column align-center">
+      <p className="title notoBold fs-28">{title}</p>
+      <div className="line" />
+      <p align="center" className="desc notoReg fs-18">
+        {desc}
+      </p>
+    </div>
+  );
+}
+
+HomeDescCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  desc: PropTypes.string.isRequired
+};
 
 export default Home;
