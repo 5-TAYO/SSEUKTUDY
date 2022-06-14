616d6fd1b2230b2071282d6b431e24e4e055aeb6
Wise Programmer <67681207+gandi0330@users.noreply.github.com>
2022-06-14 23:44:12 +0900

    setup : backend setup

---
+++ backend/src/main/java/tayo/sseuktudy/SseuktudyApplication.java
@@ -0,0 +1,13 @@
+package tayo.sseuktudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SseuktudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SseuktudyApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/tayo/sseuktudy/SseuktudyApplicationTests.java
@@ -0,0 +1,13 @@
+package tayo.sseuktudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SseuktudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
