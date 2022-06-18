129a27b96d53ca4e2ee5567dcf670911108c70b2
choijinhap <wlsgkq123@gmail.com>
2022-06-18 14:57:41 +0900

    보낸쪽지함 , 스터디단체쪽지함 퍼블리싱 #8

---
+++ frontend/src/components/Send.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Recv.scss";
+
+function Send() {
+  return (
+    <div className="recv-wrapper">
+      <div className="recv-header">
+        <div className="recv-header-left">
+          <h2 className="notoMid fs-28">보낸 쪽지함</h2>
+        </div>
+        <div className="recv-header-right">
+          <button type="button">삭제</button>
+          <button type="button">보관</button>
+        </div>
+      </div>
+      <div className="notes-list">
+        <div className="note note-list-header">
+          <div>
+            <input type="checkbox" />
+          </div>
+          <p>받은 사람</p>
+          <p>내용</p>
+          <p>날짜</p>
+        </div>
+        <div className="note">
+          <div>
+            <input type="checkbox" />
+          </div>
+          <p>jmlee9707</p>
+          <p>lorem loremloremlorem lorem lorem</p>
+          <p>22-04-27 [23:22]</p>
+        </div>
+        <div className="note unread">
+          <div>
+            <input type="checkbox" />
+          </div>
+          <p>jmlee9707</p>
+          <p>lorem loremloremlorem lorem lorem</p>
+          <p>22-04-27 [23:22]</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Send;
