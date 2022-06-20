e1d1ad7ac2c3bec0eca9ca4ac3da8ba1380f40b7
lion1735 <76879122+lion1735@users.noreply.github.com>
2022-06-20 15:07:42 +0900

    Revert "feat : 메일 발송API (#35)" (#36)
    This reverts commit 9dd3b5aeef12a781fd4bd59f76ecde59c7182596.

--- frontend/src/screens/Home.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Home.scss";
-
-function Home() {
-  return <div>Home</div>;
-}
-
-export default Home;
--- frontend/src/screens/MyPage.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./MyPage.scss";
-
-function MyPage() {
-  return <div>마이페이지 구현해주세요--!</div>;
-}
-
-export default MyPage;
