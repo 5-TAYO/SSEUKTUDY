ae79611b85f0bf45f6e069266885600b82281ac4
Yeom-JinHo <pwlsghq@naver.com>
2022-06-21 14:23:58 +0900

    refactor : 라우팅 정리

--- frontend/src/routers/Router.jsx
+++ frontend/src/routers/Router.jsx
@@ -21,27 +21,38 @@ function Router() {
     <>
       <MainNavBar />
       <Routes>
+        {/* 홈 & 메인 */}
         <Route path="/" element={<Home />} />
         <Route path="/main" element={<Main />} />
-        <Route path="/study/list" element={<StudyList />} />
-        <Route path="/study/detail/:id" element={<StudyDetail />} />
-        <Route
-          path="/study/join/:id"
-          element={<StudyRegistForm type="regist" />}
-        />
-        <Route
-          path="/study/join/read/:id" // fix__ 진합이형이 라우팅해주세요
-          element={<StudyRegistForm type="read" />}
-        />
+
+        {/* 스터디 */}
+        <Route path="study/*">
+          <Route path="list" element={<StudyList />} />
+          <Route path="detail/:id" element={<StudyDetail />} />
+          <Route path="join/:id" element={<StudyRegistForm type="regist" />} />
+          <Route
+            path="join/read/:id" // fix__ 진합이형이 라우팅해주세요
+            element={<StudyRegistForm type="read" />}
+          />
+        </Route>
+
+        {/* 로그인 */}
         <Route path="/login" element={<Login />} />
-        <Route path="/mypage/*" element={<MyPage />} />
-        <Route path="/letters/*" element={<Letters />} />
-        <Route path="/join" element={<Join />} />
-        <Route path="/join/mail" element={<JoinMail />} />
-        <Route path="/join/userinfo" element={<JoinUserInfo />} />
-        <Route path="/join/finish" element={<JoinFinish />} />
-        <Route path="/join/add" element={<JoinAdd />} />
         <Route path="/find/pw" element={<FindPw />} />
+        {/* 마이페이지 */}
+        <Route path="/mypage/*" element={<MyPage />} />
+
+        {/* 쪽지함 */}
+        <Route path="/letters/*" element={<Letters />} />
+
+        {/* 회원가입 */}
+        <Route path="/join/*">
+          <Route index element={<Join />} />
+          <Route path="mail" element={<JoinMail />} />
+          <Route path="userinfo" element={<JoinUserInfo />} />
+          <Route path="finish" element={<JoinFinish />} />
+          <Route path="add" element={<JoinAdd />} />
+        </Route>
       </Routes>
     </>
   );
