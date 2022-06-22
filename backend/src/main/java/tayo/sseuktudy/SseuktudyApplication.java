c754966aa7a567e729221e85bbb9661a5536457d
lion1735 <lion1735@naver.com>
2022-06-22 13:19:58 +0900

    feat : AWS 배포 목적 환경세팅 #43

--- backend/src/main/java/tayo/sseuktudy/SseuktudyApplication.java
+++ backend/src/main/java/tayo/sseuktudy/SseuktudyApplication.java
@@ -2,10 +2,15 @@ package tayo.sseuktudy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SseuktudyApplication {
-
+public class SseuktudyApplication extends SpringBootServletInitializer {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(SseuktudyApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SseuktudyApplication.class, args);
 	}
