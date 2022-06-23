81567eb6ad29a65b0207dd9761e5d06d9d9a3a7a
jmlee9707 <jmlee9707@naver.com>
2022-06-24 02:57:12 +0900

    fix : 글자 색상 강조(#65)

--- frontend/src/screens/join/JoinFinish.jsx
+++ frontend/src/screens/join/JoinFinish.jsx
@@ -9,16 +9,17 @@ function JoinFinish() {
       <div className="flex align-center justify-center">
         <img src={FinalIcon} alt="가입완료" className="join_finish_image" />
       </div>
-      <div className="finish_text flex column align-center justify-center">
+      <div className="finish_text">
         <div className="finish_text_title flex align-center justify-center notoBold fs-28">
           [닉네임] 님, 환영합니다.
           <br /> 회원가입을 완료하였습니다!
         </div>
-        <div className="finish_text_desc flex align-center justify-center notoReg fs-16">
-          이제 스윽터디에서 다양한 팀원들과 함께 <br />
-          목표를 잡고 꾸준하게 공부해보세요.
-          <br />
-          목표까지, 스윽터디가 함께 할게요!
+        <div className="finish_text_desc flex column align-center justify-center notoReg fs-16">
+          <p>이제 스윽터디에서 다양한 팀원들과 함께</p>
+          <p> 목표를 잡고 꾸준하게 공부해보세요.</p>
+          <p>
+            목표까지, <span className="notoBold">스윽터디</span>가 함께 할게요!
+          </p>
         </div>
       </div>
       <div className="flex column align-center justify-center">
