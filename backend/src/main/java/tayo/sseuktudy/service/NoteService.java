41d7f5ead0f4d983092843bcce8b00f1f50f49d7
lion1735 <76879122+lion1735@users.noreply.github.com>
2022-06-24 00:19:34 +0900

    feat : 쪽지 리스트 조회 API (#64)
    * feat: 쪽지 리스트 조회 - 보낸 쪽지 API 구현 완료  #58
    
    * feat: 쪽지 리스트 조회 - 받은 쪽지 API 구현 완료  #58

--- backend/src/main/java/tayo/sseuktudy/service/NoteService.java
+++ backend/src/main/java/tayo/sseuktudy/service/NoteService.java
@@ -1,7 +1,12 @@
 package tayo.sseuktudy.service;
 
+import tayo.sseuktudy.dto.note.NoteInfoDto;
 import tayo.sseuktudy.dto.note.NoteRegistDto;
+
+import java.util.List;
 
 public interface NoteService {
     public int registNote(NoteRegistDto noteRegistDto);
+    public List<NoteInfoDto> listSendNote(String userId);
+    public List<NoteInfoDto> listReciveNote(String userId);
 }
