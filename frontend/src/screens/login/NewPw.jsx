4eb47317d3eb51593f33edde8b6de40d489e1af1
jmlee9707 <jmlee9707@naver.com>
2022-06-24 02:10:39 +0900

    fix : 전체 margin 통일(#66)

--- frontend/src/screens/login/NewPw.jsx
+++ frontend/src/screens/login/NewPw.jsx
@@ -5,17 +5,17 @@ import "./NewPw.scss";
 function NewPw() {
   return (
     <div id="new_pw">
-      <div className="info_text">
-        <p className="info_text_title notoBold fs-24">
+      <div className="new_pw_text">
+        <p className="new_pw_text_title notoBold fs-24">
           새로운 비밀번호를 입력해주세요 :)
         </p>
       </div>
 
-      {/* site info input start */}
-      <div className="info_input flex column align-center">
+      {/* site new_pw input start */}
+      <div className="new_pw_input flex column align-center">
         {/* password input start */}
-        <div className="info_input_box">
-          <p className="info_input_title notoMid fs-16">비밀번호</p>
+        <div className="new_pw_input_box">
+          <p className="new_pw_input_title notoMid fs-16">비밀번호</p>
           <div className=" flex align-center justify-center">
             <input
               type="text"
@@ -24,7 +24,7 @@ function NewPw() {
             />
           </div>
           {/* 유효성 검사 start */}
-          <div className="info_input_failed notoMid fs-12">
+          <div className="new_pw_input_failed notoMid fs-12">
             대소문자, 특수문자를 혼합하여 8~16자리로 입력해주세요.
           </div>
           {/* 유효성 검사 end */}
@@ -33,8 +33,8 @@ function NewPw() {
 
         {/* password again input start */}
 
-        <div className="info_input_box">
-          <p className="info_input_title notoMid fs-16">비밀번호 확인</p>
+        <div className="new_pw_input_box">
+          <p className="new_pw_input_title notoMid fs-16">비밀번호 확인</p>
           <div className=" flex align-center justify-center">
             <input
               type="text"
@@ -43,25 +43,25 @@ function NewPw() {
             />
           </div>
           {/* 유효성 검사 start */}
-          <div className="info_input_failed notoMid fs-12">
+          <div className="new_pw_input_failed notoMid fs-12">
             비밀번호가 일치하지 않습니다.
           </div>
           {/* 유효성 검사 end */}
         </div>
         {/* password agagin input end  */}
       </div>
-      {/* site info input end */}
+      {/* site new_pw input end */}
 
-      {/* info btn start */}
+      {/* new_pw btn start */}
       <div className="flex align-center justify-center">
         <Link
           to="/main"
-          className="join_info_next_btn notoMid fs-15 flex align-center justify-center"
+          className="join_new_pw_next_btn notoMid fs-15 flex align-center justify-center"
         >
           비밀번호 재설정
         </Link>
       </div>
-      {/* info btn end */}
+      {/* new_pw btn end */}
     </div>
   );
 }
