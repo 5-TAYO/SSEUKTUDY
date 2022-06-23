71292db200107d4c14f574054059c486ff6e5cf9
choijinhap <wlsgkq123@gmail.com>
2022-06-23 22:50:10 +0900

    feat - 모달 컴포넌트 , 스타일링 #61

---
+++ frontend/src/components/common/Modal.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import "./Modal.scss";
+
+function Modal({ open, close, header, children, footerBtn }) {
+  return (
+    <div className={open ? "openModal modal" : "modal"}>
+      {open && (
+        <section>
+          <header>
+            {header}
+            <button type="button" className="close" onClick={close}>
+              &times;
+            </button>
+          </header>
+          <main>{children}</main>
+          <footer>
+            {footerBtn}
+            <button type="button" className="close" onClick={close}>
+              close
+            </button>
+          </footer>
+        </section>
+      )}
+    </div>
+  );
+}
+Modal.defaultProps = {
+  header: null,
+  children: null,
+  footerBtn: null
+};
+Modal.propTypes = {
+  open: PropTypes.bool.isRequired,
+  close: PropTypes.func.isRequired,
+  header: PropTypes.element,
+  children: PropTypes.element,
+  footerBtn: PropTypes.element
+};
+export default Modal;
